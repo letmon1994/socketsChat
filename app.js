105e41efd11be6e65b14a617ee61a1f8674537c4
Ryan Letmon <N00152299@iadt.ie>
2018-02-07 17:17:24 +0000

    lastest one

--- app.js
+++ app.js
@@ -2,19 +2,25 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
 
-io.emit('some event', { for: 'everyone' });
-
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
+io.emit('some event', {
+    for: 'everyone'
 });
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+io.on('connection', function (socket) {
+    io.emit('conn');
+    socket.on('disconnect', function () {
+        io.emit('disc');
+    });
+    socket.on('chat message', function (msg) {
+        io.emit('chat message', msg);
+    });
+
 });
-    
+
+http.listen(3000, function () {
+    console.log('listening on *:3000');
+});
