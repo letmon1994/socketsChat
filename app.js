1bebd9e64354ec0c1d42fd05f26ec277ccdfae5d
Ryan Letmon <N00152299@iadt.ie>
2018-02-07 16:16:24 +0000

    last add to github

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+io.emit('some event', { for: 'everyone' });
+
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function(){
-    console.log('user disconnected');
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
